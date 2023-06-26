a01670b2b2e2559f32ad82c11192ddc9acfd0902
henzo <84385799+henzopuchetti@users.noreply.github.com>
2023-06-26 09:54:51 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Aiai from './assets/Aiai.png'
+import './Oi.css'
+
+function App() {
+  return (
+    <>
+    <div className="fundo2">
+      <h2>Nossa História</h2>
+      <p>No cenário da tecnologia moderna, a combinação de conhecimento em processamento de dados e a preocupação com a saúde e o bem-estar tem levado a grandes avanços. Um exemplo notável disso é um grupo de seis estudantes de processamento de dados que se uniram para criar um aplicativo revolucionário voltado para mulheres na menopausa.
+Os estudantes, Alice, Bruno, Clara, Diego, Eva e Felipe, compartilhavam o interesse comum em desenvolver soluções inovadoras para melhorar a qualidade de vida das pessoas. Eles reconheceram que a menopausa é um período de transição importante na vida das mulheres, e muitas delas enfrentam desafios relacionados à nutrição durante essa fase. Assim, decidiram concentrar seus esforços em criar um aplicativo que fornecesse orientação e suporte nutricional personalizado para mulheres na menopausa.</p>
+    </div>
+    <img src={Aiai} className='Aiai'/>
+    </>
+  )
+}
+
+export default App
